c15fbabaae2385ed98845d99d79d5e0cbaed07a3
abel.rutto@student.moringaschool.com <abel.rutto@student.moringaschool.com>
2023-02-11 11:14:36 +0300

    completed

--- spec/email_parser_spec.rb
+++ spec/email_parser_spec.rb
@@ -24,3 +24,12 @@ describe "EmailAddressParser" do
     end
   end
 end
+class EmailAddressParser
+  attr_accessor :emails
+  def initialize(emails)
+      @emails = emails
+  end
+  def parse
+      emails.split(/, | /).uniq
+  end
+end
